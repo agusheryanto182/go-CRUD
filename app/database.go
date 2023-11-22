9cae5e2e4ce94361400b1db52b5763b67af6be2f
Agus Heryanto <agusheryantodev@gmail.com>
2023-11-22 21:05:37 +0700

    add : helper

--- app/database.go
+++ app/database.go
@@ -1,1 +1,31 @@
 package app
+
+import (
+	"database/sql"
+	"fmt"
+	"time"
+
+	"github.com/agusheryanto182/go-CRUD/models/config"
+	_ "github.com/go-sql-driver/mysql"
+)
+
+func NewDB(c *config.Global) *sql.DB {
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?parseTime=true",
+		c.Database.User,
+		c.Database.Password,
+		c.Database.Host,
+		c.Database.Port,
+		c.Database.Name)
+
+	db, err := sql.Open("mysql", dsn)
+	if err != nil {
+		panic(err)
+	}
+
+	db.SetMaxIdleConns(10)
+	db.SetMaxOpenConns(100)
+	db.SetConnMaxIdleTime(5 * time.Minute)
+	db.SetConnMaxLifetime(60 * time.Minute)
+
+	return db
+}
--- models/domain/book.go
+++ models/domain/book.go
@@ -1,8 +1,12 @@
 package model
 
+import "time"
+
 type Book struct {
-	Id     int
-	Title  string
-	Author string
-	Year   int
+	Id        int
+	Title     string
+	Author    string
+	Year      int
+	CreatedAt time.Time
+	UpdatedAt time.Time
 }
